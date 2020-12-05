8819e10207cf29d33b72772d05b14fc12295edbf
Eduardo Gil <pisangas@gmail.com>
2020-12-04 20:27:52 -0500

    peticion API

---
+++ AppXam/AppXam/Services/ApiPersonas.cs
@@ -0,0 +1,41 @@
+﻿using AppXam.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppXam.Services
+{
+    public class ApiPersonas
+    {
+        public const string urlApi = "https://webapixamarin.azurewebsites.net/api/Personas";
+        public async Task<IList<Persona>> ObtenerPersonas()
+        {
+            List<Persona> personas = new List<Persona>();
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                httpClient.MaxResponseContentBufferSize = 256000;
+
+                var uri = new Uri(urlApi);
+                var response = await httpClient.GetAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return ApiPersonas;
+
+        }
+
+
+    }
+}
