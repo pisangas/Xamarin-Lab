cafc24b181757ee277afccb29a53cd4f32c84392
Eduardo Gil <pisangas@gmail.com>
2020-12-04 19:36:32 -0500

    Inicial

---
+++ AppXam/AppXam/Modelos/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppXam.Modelos
+{
+    public class Persona
+    {
+        public int PersonaId { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
